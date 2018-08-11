abee97cf4243c2cef707da5054d519a9bbb626e0
Kieran Bamford <kieran.bamford@gmail.com>
2018-08-11 23:52:50 +0100

    Added author selection screen
    removed option for title search for now

--- GoodQuotes/Services/QuoteService.swift
+++ GoodQuotes/Services/QuoteService.swift
@@ -10,6 +10,7 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
+//TODO: This is a mess already, refactor it
 class QuoteService {
     let baseUrl = "https://goodquotesapi.herokuapp.com"
     let authorPath = "/author"
@@ -21,35 +22,70 @@ class QuoteService {
         let defaultsService = UserDefaultsService()
         let settings = defaultsService.loadFilters()
         
-        let randomPage = Int(arc4random_uniform(UInt32(100)))
         if settings == nil || settings?.type == FilterType.None
         {
-            let author = randomAuthor()
-            getAllQuotesForAuthorAtPage(author: "\(author)", pageNumber: randomPage) { quotes in
-                if quotes.count == 0 {
-                    self.getRandomQuote(completion: { quote in
-                        completion(quote)
-                    })
-                }
-                else {
-                    let random = Int(arc4random_uniform(UInt32(quotes.count)))
-                    completion(quotes[random])
-                }
-            }
+            getFullyRandomQuote(completion: completion)
             return
         }
         
         if settings?.type == FilterType.Tag {
-            getAllQuotesForTagAtPage(tag: settings!.filter, pageNumber: randomPage) { quotes in
-                if quotes.count == 0 {
-                    self.getRandomQuote(completion: { quote in
-                        completion(quote)
-                    })
-                }
-                else {
-                    let random = Int(arc4random_uniform(UInt32(quotes.count)))
-                    completion(quotes[random])
-                }
+            getTagQuotes(filter:settings!.filter, completion: completion)
+            return
+        }
+        
+        if settings?.type == FilterType.Author {
+            getAuthorQuote(author:settings!.filter, completion: completion)
+            return
+        }
+    }
+    
+    internal func getFullyRandomQuote(completion: @escaping (Quote) -> ())
+    {
+        let randomPage = Int(arc4random_uniform(UInt32(100)))
+        let author = randomAuthor()
+        getAllQuotesForAuthorAtPage(author: "\(author)", pageNumber: randomPage) { quotes in
+            if quotes.count == 0 {
+                self.getRandomQuote(completion: { quote in
+                    completion(quote)
+                })
+            }
+            else {
+                let random = Int(arc4random_uniform(UInt32(quotes.count)))
+                completion(quotes[random])
+            }
+        }
+    }
+    
+    internal func getAuthorQuote(author: String, completion: @escaping (Quote) -> ())
+    {
+        let randomPage = Int(arc4random_uniform(UInt32(100)))
+        let editedAuthor = author.components(separatedBy: .whitespaces).joined(separator: "+")
+        
+        getAllQuotesForAuthorAtPage(author: "\(editedAuthor)", pageNumber: randomPage) { quotes in
+            if quotes.count == 0 {
+                self.getFullyRandomQuote(completion: { quote in
+                    completion(quote)
+                })
+            }
+            else {
+                let random = Int(arc4random_uniform(UInt32(quotes.count)))
+                completion(quotes[random])
+            }
+        }
+    }
+    
+    internal func getTagQuotes(filter: String, completion: @escaping (Quote) -> ())
+    {
+        let randomPage = Int(arc4random_uniform(UInt32(100)))
+        getAllQuotesForTagAtPage(tag: filter, pageNumber: randomPage) { quotes in
+            if quotes.count == 0 {
+                self.getFullyRandomQuote(completion: { quote in
+                    completion(quote)
+                })
+            }
+            else {
+                let random = Int(arc4random_uniform(UInt32(quotes.count)))
+                completion(quotes[random])
             }
         }
     }
