6e3ed94eef9be3e2ab427b35272a2d1262e97bb1
Kieran Bamford <kieran.bamford@gmail.com>
2018-08-16 23:21:12 +0100

    Storing oAuth token/secret for reuse

--- GoodQuotes/Services/GoodreadsService.swift
+++ GoodQuotes/Services/GoodreadsService.swift
@@ -13,9 +13,10 @@ import OAuthSwift
 
 class GoodreadsService {
     var oauthswift: OAuthSwift?
+    var id: String?
     
-    func loginToGoodreadsAccount(sender: UIViewController)
-    {
+    func loginToGoodreadsAccount(sender: UIViewController) {
+        
         let oauthswift = OAuth1Swift(
             consumerKey:        Bundle.main.localizedString(forKey: "goodreads_key", value: nil, table: "Secrets"),
             consumerSecret:     Bundle.main.localizedString(forKey: "goodreads_secret", value: nil, table: "Secrets"),
@@ -23,22 +24,50 @@ class GoodreadsService {
             authorizeUrl:       "https://www.goodreads.com/oauth/authorize?mobile=1",
             accessTokenUrl:     "https://www.goodreads.com/oauth/access_token"
         )
+        
         self.oauthswift=oauthswift
         oauthswift.allowMissingOAuthVerifier = true
         oauthswift.authorizeURLHandler = SafariURLHandler(viewController: sender, oauthSwift: self.oauthswift!)
         
-        let _ = oauthswift.authorize(
-            withCallbackURL: URL(string: "Quotey://oauth-callback/goodreads")!,
-            success: { credential, response, parameters in
-                let test = response
-                print(credential)
-                print(response)
-                //TODO: Handle login response (save user ID etc)
-        },
-            failure: { error in
-                print( "ERROR ERROR: \(error.localizedDescription)", terminator: "")
+        let authToken = AuthStorageService.readAuthToken()
+        let authSecret = AuthStorageService.readTokenSecret()
+        
+        if authToken.isEmpty || authSecret.isEmpty{
+            let _ = oauthswift.authorize(
+                withCallbackURL: URL(string: "Quotey://oauth-callback/goodreads")!,
+                success: { credential, response, parameters in
+                    AuthStorageService.saveAuthToken(credential.oauthToken)
+                    AuthStorageService.saveTokenSecret(credential.oauthTokenSecret)
+                    self.loginToUser(oauthswift)
+            },
+                failure: { error in
+                    print( "ERROR ERROR: \(error.localizedDescription)", terminator: "")
+            })
         }
-        )
+        else {
+            oauthswift.client.credential.oauthToken = authToken
+            oauthswift.client.credential.oauthTokenSecret = authSecret
+            
+            loginToUser(oauthswift)
+        }
     }
     
+    func loginToUser(_ oauthswift: OAuth1Swift) {
+        let _ = oauthswift.client.get(
+            "https://www.goodreads.com/api/auth_user",
+            success: { response in
+                let xml = try! XML.parse(response.string!)
+                guard let id = xml["GoodreadsResponse", "user"].attributes["id"] else {
+                    return
+                }
+                self.id = id
+                
+        }, failure: { error in
+            print(error)
+        })
+    }
+    
+    func loadShelves() {
+        
+    }
 }
