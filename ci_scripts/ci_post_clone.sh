555d7efe04819b8e6dcf5655a3eeefa2884359a2
Kieran Bamford <2924326+Protome@users.noreply.github.com>
2024-06-15 15:13:14 +0100

    removes unnecessary part of script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,5 +8,4 @@
 # Install dependencies you manage with CocoaPods.
 #pod install
 
-cd $CI_WORKSPACE/ci_scripts || exit 1
-printf "\"goodreads_key\":\"%s\"; \n\"goodreads_secret\":\"%s\";" "$GOODREADS_KEY" "$GOODREADS_SECRET" >> ../GoodQuotes/Secrets.Strings
+printf "\"goodreads_key\"=\"%s\"; \n\"goodreads_secret\"=\"%s\";" "$GOODREADS_KEY" "$GOODREADS_SECRET" >> ../GoodQuotes/Secrets.Strings
